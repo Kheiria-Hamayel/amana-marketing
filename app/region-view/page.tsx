38cba72c00061e41c29600ce1ef33be8034f3269
Kheiria-Hamayel <khiriyya123.amjad@gmail.com>
2025-10-10 21:26:10 +0300

    fix 401 error p3

--- app/region-view/page.tsx
+++ app/region-view/page.tsx
@@ -180,12 +180,10 @@ const LeafletBubbleMap: React.FC<LeafletBubbleMapProps> = ({
 
         mapInstanceRef.current = map;
 
-        // Using CartoDB Dark Matter tiles which do not require an API key
         L.tileLayer(
-          "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+          "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
           {
-            attribution:
-              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            attribution: "© OpenStreetMap contributors, © CartoDB",
             maxZoom: 20,
           }
         ).addTo(map);
